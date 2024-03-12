abfda0d6d56cba529ba38f56fc0d532e3a121f5a
Sajad <sajjadoncall@gmail.com>
2024-03-12 10:12:56 -0400

    feat:add logging to update document and support for large items

--- utils/update_documents.py
+++ utils/update_documents.py
@@ -1,58 +1,73 @@
-
-
 import boto3 
 import datetime
 from process_text import process_url
 from datetime import datetime
 import asyncio
 from dateutil.parser import parse
+import json
 
-    
+# Importing Logger
+from aws_lambda_powertools import Logger
+
+# Initialize Logger
+logger = Logger(service="doc_index_update_documents", level="INFO")
+
 async def update_single_document(document):
     url = document.get('url')
     doc_type = document.get('type')
     if url:
-        # Process the URL and update the text asynchronously
         new_text = await process_url(url, doc_type)
         document['text'] = new_text
 
 async def update_documents(TableName, last_updated_before=None):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(TableName)
+    processed_items = []  # List to track processed items
 
-    # Convert last_updated_before to datetime if it's a string
     if isinstance(last_updated_before, str):
         last_updated_before = datetime.fromisoformat(last_updated_before)
-    print("fksdjlfa")
-    # Scan the table
+
+    logger.info("Starting the document update process")
     response = table.scan()
     items = response['Items']
 
-    # Loop through each item
     for item in items:
         updated_at = item.get('updated_at')
         if updated_at:
             updated_at = parse(updated_at)
-
-            # Check if item was last updated before the given time
             if last_updated_before is None or updated_at < last_updated_before:
-                documents = item['documents']
+                try:
+                    documents = item['documents']
+                    await asyncio.gather(*[update_single_document(document) for document in documents])
+                    new_updated_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    # table.update_item(
+                    #     Key={'features_properties_id': item['features_properties_id']},
+                    #     UpdateExpression='SET documents = :val1, updated_at = :val2',
+                    #     ExpressionAttributeValues={
+                    #         ':val1': documents,
+                    #         ':val2': new_updated_at
+                    #     }
+                    # )
+                    item['updated_at'] = new_updated_at
+                    item['documents'] = documents
+                    table.put_item(Item=item)
+                    processed_items.append(item["features_properties_id"])
+                    logger.info(f'Updated item: {item["features_properties_id"]}')
+                except Exception as e:
+                    logger.warn(f'unsucessfull attempt for {item["features_properties_id"]} error : {e}')
+        
+    logger.info("All applicable documents updated successfully")
 
-                # Process each document asynchronously
-                await asyncio.gather(*[update_single_document(document) for document in documents])
+    # Save processed items to a JSON file
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"processed_items_{timestamp}.json"
+    with open(filename, 'w') as file:
+        json.dump(processed_items, file)
+    logger.info(f"Processed items saved to {filename}")
 
-                # Update the 'updated_at' field
-                new_updated_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # upload the file to S3
+    # s3_client = boto3.client('s3')
+    # bucket_name = 'your_bucket_name'
+    # s3_client.upload_file(filename, bucket_name, filename)
+    # logger.info(f"File uploaded to S3: {bucket_name}/{filename}")
 
-                # Update the item in the DynamoDB table
-                table.update_item(
-                    Key={'features_properties_id': item['features_properties_id']},
-                    UpdateExpression='SET documents = :val1, updated_at = :val2',
-                    ExpressionAttributeValues={
-                        ':val1': documents,
-                        ':val2': new_updated_at
-                    }
-                )
-                print('updated {}'.format(item["features_properties_id"]))
-    
-    print("All applicable documents updated successfully!")
